7b8d99eb485b287d2468c8d4c64d1ea698733ab8
James Sapp <jameseisapp@gmail.com>
2021-01-05 10:43:38 -0600

    seed changes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,12 @@
 class Recipe < ActiveRecord::Base 
     has_many :users
+
+
+    def meal_category
+        category_data = RestClient.get('https://www.themealdb.com/api/json/v2/9973533/categories.php')
+        parsed_categroy = JSON.parse(category_data)
+        binding.pry 
+    end 
+
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,17 @@
 User.destroy_all
 Recipe.destroy_all
-
+Favorite.destroy_all
 
 10.times do
-
     User.create(name: Faker::FunnyName.two_word_name)
 
-    recipe_data = RestClient.get('https://www.themealdb.com/api/json/v1/1/random.php')
+    recipe_data = RestClient.get('https://www.themealdb.com/api/json/v2/9973533/random.php')
     parsed_recipe = JSON.parse(recipe_data)
     meal_hash = parsed_recipe["meals"][0]
     meal_name = meal_hash["strMeal"]
     meal_category = meal_hash["strCategory"]
     meal_instructions = meal_hash["strInstructions"]
     meal_area = meal_hash["strArea"]
-
     Recipe.create(category: meal_category, name: meal_name, region: meal_area, instruction: meal_instructions)
 end
 
@@ -24,4 +22,4 @@ recipe_id = Recipe.all.map { |recipe| recipe.id }
     Favorite.create(user_id: user_id.sample, recipe_id: recipe_id.sample)
 end 
 
-binding.pry
+#binding.pry
