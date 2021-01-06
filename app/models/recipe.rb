78db607775a8e73d6b8cc9fbf89f1aeb3f950631
edougher <ejdougher@gmail.com>
2021-01-05 20:32:57 -0500

    Recipe methods added

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Recipe < ActiveRecord::Base 
     # has_many :users
     has_many :favorites
@@ -5,9 +6,30 @@ class Recipe < ActiveRecord::Base
 
     # methods needed:
     # display all caterogies in an array
-    # display all ingredients with measurements 
+    def self.all_categories
+        self.all.map {|recipe| recipe.category}.uniq
+    end
+    # display all ingredients with measurements
+    def self.all_ingredients 
+        self.all.map {|recipe| recipe_name.ingredient_list}
+    end
     # sort recipes by time under 30, under 45, under 60
+    def self.sort_by_cooktime
+        self.all.map do |recipe| 
+        "#{recipe.name} takes #{recipe.time} minutes"
+          #less than 20
+        #case recipe.time 
+        #when 0..20
+        #    "#{recipe.name} takes 20 minutes or less"
+        # #between 20 and 40
+        #when 21..40
+        #    "#{recipe.name} takes less than 40 minutes"
+        #else 
+        #    "#{recipe.name} takes #{recipe.time} minutes"
+        #end
+      end
+    end
+
     # find all the users 
-
-
+    
 end
